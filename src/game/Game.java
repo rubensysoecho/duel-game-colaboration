6c52dc3ce2b22366d78dbc72e11c5b58bf1d7196
Rubén <95583458+rubensysoecho@users.noreply.github.com>
2022-04-06 11:16:13 +0200

    main menu (not finished)

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,7 +1,11 @@
 package game;
 
+import java.util.Scanner;
+
 public class Game {
 
+	public static final Scanner input = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 
 //		intro();
@@ -12,15 +16,38 @@ public class Game {
 //		tu.stats();
 
 		// Player has 10 point to assign
+		main_menu();
 		Player player = new Player("Maxim", 3, 3, 2, 1, 1);
 		player.showStats();
 
-		Enemy enemy = new Enemy("Oscar", 1, 4, 5, 0, 0);
+		Enemy enemy = new Enemy("ENEMY", 1, 4, 5, 0, 0);
 		enemy.showStats();
 
 		Duel duel = new Duel(player, enemy);
 
 		duel.start();
+	}
+	
+	public static void main_menu()	{
+		System.out.println("@@@@@@@@@@@@@@@@@");
+		System.out.println("@               @");
+		System.out.println("@   DUEL GAME   @");
+		System.out.println("@               @");
+		System.out.println("@@@@@@@@@@@@@@@@@");
+		System.out.println();
+		System.out.println("#################");
+		System.out.println("#################");
+		System.out.println();
+		System.out.println(" ---------------------------------------");
+		System.out.println("--------CREADOR DE PERSONAJES------------");
+		System.out.println(" ---------------------------------------");
+		System.out.print("|Nombre: ");
+		String name = input.next();
+		System.out.println("|----TIENES 10 PUNTOS PARA DISTRIBUIR----|");
+		System.out.println("|Vida--> ");
+		System.out.println("");
+		System.out.println();
+		
 	}
 
 //	public final Scanner entrada = new Scanner(System.in);
